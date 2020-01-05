d694d43fed430674e3d8577e32f816dd51bef188
Paweł Bogdan <pawel.bogdan1@gmail.com>
2020-01-05 16:49:31 +0100

    Kilka featerów

--- src/test/java/pl/altkom/tests/aritmetics/FloatArithmeticTest.java
+++ src/test/java/pl/altkom/tests/aritmetics/FloatArithmeticTest.java
@@ -1,15 +1,53 @@
 package pl.altkom.tests.aritmetics;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import pl.altkom.tests.exceptions.CalculatorException;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FloatArithmeticTest {
     @Test
-    public void simpleTest() throws CalculatorException {
+    @Disabled
+    public void simpleAdditionTest() throws CalculatorException{
         FloatArithmetic a = new FloatArithmetic();
-        assertEquals("4.0", a.add("2.0", "2"));
+        assertEquals("4.1",a.add("2","2"));
+        assertNotEquals("4.0", a.add("2", "2"));
     }
 
+    @Test
+    @Disabled
+    public void additionTest() throws CalculatorException{
+        FloatArithmetic a = new FloatArithmetic();
+        assertAll("dodawanie",
+                () -> assertEquals("4.1",a.add("2","2") ),
+                () -> assertNotEquals("4.0", a.add("2", "2")));
+    }
+
+    @Test
+    public void parseFailedError() {
+        FloatArithmetic a = new FloatArithmetic();
+        assertThrows(CalculatorException.class,
+                () -> a.add("nic", "hahah"));
+    }
+
+    @Tag("dobry_test")
+    @DisplayName("dobry_test")
+    @Test
+    public void SubtractionTest(TestInfo info, TestReporter reporter) throws CalculatorException {
+        FloatArithmetic a = new FloatArithmetic();
+        assertEquals("3.0", a.sub("6","3"));
+        System.out.println(info.toString());
+        reporter.publishEntry("Klucz", "Wartość");
+    }
+
+    @RepeatedTest(20)
+    public void repeatTest(TestReporter reporter, RepetitionInfo info) {
+        reporter.publishEntry(
+                String.valueOf(info.getCurrentRepetition()),
+                String.valueOf(info.getTotalRepetitions()));
+        Random r = new Random();
+        if (r.nextBoolean()) fail();
+    }
 }
